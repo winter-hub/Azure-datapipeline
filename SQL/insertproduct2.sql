5a3d7f50144e920aaf380187e13e1cbd276721d1
winter-hub <67466252+winter-hub@users.noreply.github.com>
2025-09-25 07:44:46 +0530

    Upload query for inserting new product in dataset

---
+++ SQL/insertproduct2.sql
@@ -0,0 +1,25 @@
+USE [AdventureWorksLT2022]
+GO
+
+INSERT INTO [SalesLT].[Product]
+           ([Name]
+           ,[ProductNumber]
+           ,[Color]
+           ,[StandardCost]
+           ,[ListPrice]
+           ,[Size]
+           ,[Weight]
+           ,[ProductCategoryID]
+           ,[ProductModelID]
+           ,[SellStartDate]
+           ,[SellEndDate]
+           ,[DiscontinuedDate]
+           ,[ThumbNailPhoto]
+           ,[ThumbnailPhotoFileName]
+           ,[rowguid]
+           ,[ModifiedDate])
+     VALUES
+           ('Product Name1', 'PN-0012', 'Blue', 30.00, 100.00, 'M', 1.5, 3, 12, '2024-08-25', NULL, NULL, 0x, 'thumb.jpg', NEWID(), GETDATE())
+GO
+
+
